3c2e955857ded5cbd36f632ffa56eeb22e1013dd
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2021-10-18 20:18:54 -0700

    finished up form, added button, drafted handleSubmit, drafted getProfile, drafted createProfile both in fetchUtils - needs review, broken because auth not set up yet? No token yet

--- src/fetch-utils.js
+++ src/fetch-utils.js
@@ -1,7 +1,8 @@
 import request from 'superagent';
-//const URL = 'https://dog-match-be.herokuapp.com/';
-const URL = 'http://localhost:7890/';
+const URL = 'https://dog-match-be.herokuapp.com/';
+// const URL = 'http://localhost:7890/';
 
+// Login
 export async function login(email, password){
   const response = await request 
     .post(`${URL}auth/signin`)
@@ -9,7 +10,8 @@ export async function login(email, password){
   
   return response.body;
 }
-  
+
+// Sign Up
 export async function signUp(email, password){
   const response = await request 
     .post(`${URL}auth/signup`)
@@ -17,3 +19,24 @@ export async function signUp(email, password){
   
   return response.body;
 }
+
+// Get Profile
+//  Does URL/api work or route to profilePage.js?
+export async function getProfile(token){
+  const response = await request 
+    .get(`${URL}api`)
+    .set('Authorization', token);
+  return response.body;
+}
+
+// Create (update) Profile
+// check .post url
+export async function createProfile(state, token){
+  const response = await request 
+    .post (`${URL}api/`)
+    .send ({ userData: state })
+    .set('Authorization', token);
+  return response.body;
+}
+
+//  ^^ what about isLoading?
