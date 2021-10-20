91a876a4c66dc91654ae842abcafd0799ae0b42c
AmitSheth9 <amit.sheth9@gmail.com>
2021-10-20 14:55:06 -0700

    added favorite button to results that is removed when added

--- src/components/BreedCard.js
+++ src/components/BreedCard.js
@@ -1,7 +1,29 @@
 import React, { Component } from 'react';
+import { getFavorites, postFavorite } from '../fetch-utils';
 import '../styles/BreedCard.css';
 
 export default class BreedCard extends Component {
+  state = {
+    favorites: []
+  }
+  componentDidMount = async () => {
+    const favorites = await getFavorites(this.props.token);
+    this.setState({ favorites });
+  }
+  handleFavorite = async () => {
+    await postFavorite(this.props.name, this.props.token);
+    const favorites = await getFavorites(this.props.token);
+    this.setState({ favorites });
+    console.log(this.state.favorites);
+  }
+
+  handleDelete = async () => {
+    
+  }
+
+  checkFavorites = (name, favoritesArr) => {
+    return favoritesArr.find(favorite => favorite.name === name);
+  }
 
   render() {
     const name = this.props.name;
@@ -21,6 +43,10 @@ export default class BreedCard extends Component {
           <li>Life Span: {lifeSpan}</li>
           <li>Temperaments: {temperaments}</li>
         </ul>
+        { !this.checkFavorites(name, this.state.favorites) && 
+        <button onClick={this.handleFavorite}>Add to Favorites</button>
+        }
+        
         <a href={adoptionLink}>Adopt This breed</a>
       </div>
     );
